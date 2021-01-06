aba5e6bcad46eb29e48721d8117a277bee74933b
Momfus <momfus@outlook.com>
2021-01-06 01:22:00 -0300

    Swiper - Slideshow de películas - Clase 250

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,12 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
 
@@ -14,6 +15,15 @@ export class SlideshowComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.movies)
+  }
+
+  // Necesita primero renderizarse todo en el ngOnInit y luego traer la configuración del swipe para estructurarse correctamente
+  ngAfterViewInit(): void {
+
+    const mySwiper = new Swiper('.swiper-container', {
+
+      loop: true, // Par más argumentos opcionales, ver la documentación en https://swiperjs.com
+    });
   }
 
 }
