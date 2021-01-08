d2597bb1dd9f98fc2c6e20e5dd886736cd7a07a3
Momfus <momfus@outlook.com>
2021-01-08 00:48:15 -0300

    Prevenir múltiples llamadas al API - Clase 256

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 
@@ -9,14 +9,37 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 })
 export class HomeComponent implements OnInit {
 
-  public movies: Movie[] = [];
+  public movies: Movie[] = []; // Todas las peliculas que hay
+  public moviesSlideshow: Movie[] = []; // Películas para mostrar
+
+  @HostListener('window:scroll', ['$event']) // Esta atento aun evento (en este caso cuando se haga scroll e interesa la propiedad evento)
+  onScroll(): void { // Función realizada con el HostListener (de más arriba)
+
+    // Se realiza el cálculo donde debe realizarse el lazyload
+    // Coloca la posición de la parte superior del scrollo, de estar undefined la del body
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1300;
+    const max = ( document.documentElement.scrollHeight || document.body.scrollHeight ); // Si ambos son iguales, es que se llegó al final
+
+    if ( pos > max ) {
+
+      if ( this.peliculasService.cargando ) { return; } // Si esta cargando aún, no llama al servicio
+
+      this.peliculasService.getCartelera().subscribe(
+        (movies) => {
+          this.movies.push(...movies); // Agrega al final el nuevo resultado
+        }
+      );
+    }
+
+  }
 
   constructor( private peliculasService: PeliculasService ) {
 
     this.peliculasService.getCartelera()
-      .subscribe( res => { // No se recomienda a poner el tipado aca, sino que venga ya desde el servicio
+      .subscribe( movies => { // No se recomienda a poner el tipado aca, sino que venga ya desde el servicio
         // console.log(res);
-        this.movies = res.results;
+        this.movies = movies;
+        this.moviesSlideshow = movies;
       });
 
   }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CarteleraResponse } from '../interfaces/cartelera-response';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators'; // Tap ejecuta un código cada vez que hay el observable emite un cambio
+import { CarteleraResponse, Movie } from '../interfaces/cartelera-response';
 
 
 @Injectable({
@@ -9,12 +10,41 @@ import { CarteleraResponse } from '../interfaces/cartelera-response';
 })
 export class PeliculasService {
 
+  // tslint:disable: no-inferrable-types
+  private baseUrl: string = 'https://api.themoviedb.org/3'; // En caso de cambiar en develop y production, se define en el environment
+  private carteleraPage: number = 1;
+  public cargando: boolean = false;
+
   constructor( private http: HttpClient) { }
 
-  getCartelera(): Observable<CarteleraResponse> {
+  get params(): any {
+    return {
+      api_key: '4a80e067246de68085ecf7925f25dd6f',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    };
+  }
 
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=4a80e067246de68085ecf7925f25dd6f&language=es-ES&page=1')
+  getCartelera(): Observable<Movie[]> {
 
+    if ( this.cargando ) {
+      // Cargando películas
+      return of([]); // Simula devolver un observable
+    }
+
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`, {
+      params: this.params // Añadiendo parametros en la petición
+    }).pipe(
+      map( (res) => {  // Transformar lo de CqrteleraResponse a solo Movie (que es lo necesario)
+        return res.results;
+      }),
+      tap( () => {
+        this.carteleraPage += 1; // aumentar la página cada vez que se realiza un cambio en el llamado
+        this.cargando = false;
+      })
+    );
 
   }
 
